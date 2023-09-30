6f0c664bdf146996567cb36d15409cb9cc485b69
Manoj Paudel <mpsandip12@gmail.com>
2023-09-30 22:57:07 +0545

    Added File

--- Laptop-Price-Prediction-EDA/Laptop-Price-Prediction.R
+++ Laptop-Price-Prediction-EDA/Laptop-Price-Prediction.R
@@ -17,13 +17,10 @@ head(dataset)
 dim(dataset) # total 1302 rows and 12 columns
 
 
-glimpse(dataset) # dataset overview
-# from the dataset, we can see many column like RAM, 
-# memory, weight are in char datatype, we need to convert them 
-# into numeric datatype by removing the unit written after the value.
+glimpse(dataset) 
 
 # check if there any duplicate data present or not.
-duplicated(dataset)  # no duplicated found in dataset
+duplicated(dataset) 
 
 # Any Null value?
 anyNA(dataset) # No null value present.
@@ -54,15 +51,56 @@ glimpse(dataset)
 ## Data visualization / EDA
 # =============================
 
-# Visualize Price and TypeName
-ggplot(data = dataset,
-       mapping = aes(x = TypeName, y = Price)) +
-         geom_bar(stat="identity")
+# Which price laptop were more sold?
+hist(dataset$Price, main='Distribution of Price', xlab='Price', col='blue', breaks=12)
+
+# Which company have higher number of laptop ? - Dell, Lenovo, Hp
+ggplot(data = dataset) +
+  geom_bar(mapping = aes(x = Company))
+
+# company relationship with price 
+# which company laptop have high price?
+ggplot(dataset, aes(x = factor(Company), y = Price, fill = Price, colour = Price)) + 
+  geom_bar(stat = "identity", position = "dodge")
+# Razer laptop are more expansive laptop, then other brand
 
 
-# From barchart we can say, in comparison of others laptop's Notebook and Gaming laptops
-# have higher price.  Why?? 
-# - do they offer better CPU, more memory than other laptops??.
-# Also, Netbook and Workstation have lower price.
+# which type of laptop are available most in our dataset?
+barplot(table(dataset$TypeName), main='Type of Laptop', col='blue', breaks=12)
+# we can see, Laptop type Notebook are the most in the data set
 
+# what's the price comparison with laptop type?
+ggplot(dataset, aes(x = factor(TypeName), y = Price, fill = Price, colour = Price)) + 
+  geom_bar(stat = "identity", position = "dodge")
+# we can see, gaming laptop's are more costly than other's
+
+# what about laptop size, do laptop size will vary the price?
+hist(dataset$Inches, main='Distribution of Laptop Size', xlab='Inches', col='red', breaks=12)
+
+ggplot(dataset, aes(x = factor(Inches), y = Price, fill = Price, colour = Price)) + 
+  geom_bar(stat = "identity", position = "dodge")
+
+# From the plot we can see there is a relationship 
+# but not a strong relationship between the price and size column.
+# We can say, Size doesn't matter for laptop price.
+
+# =============================
+## Feature Engineering
+# =============================
+
+
+# Does Screen Resolution affect Price of Laptop?
+dataset$ScreenResolution
+# we can see, this column, contain a lot of information. 
+sum(duplicated(dataset$ScreenResolution))
+duplicated(dataset$ScreenResolution) # lots of duplicate value
+unique(dataset$ScreenResolution)
+
+# table(dataset$ScreenResolution) # - count values
+
+# As, we observe the unique values we can see information related 
+# are a laptop touch screen or not, the IPS panel, 
+# and the X-axis and Y-axis screen resolution.
+
+#1. We will extract touch screen information
 
