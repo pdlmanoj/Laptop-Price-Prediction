f5fe2353a2fb5ffadc96ac5cdb96f1900c7fd85b
Manoj Paudel <mpsandip12@gmail.com>
2023-10-01 23:01:59 +0545

    Added File

--- Laptop-Price-Prediction-EDA/Laptop-Price-Prediction.R
+++ Laptop-Price-Prediction-EDA/Laptop-Price-Prediction.R
@@ -107,9 +107,10 @@ unique(dataset$ScreenResolution) # only unique value (what are those value?)
 # Fetching touchscreen data from Screen resolution column
 dataset$touchscreen <- ifelse(grepl("Touchscreen", dataset$ScreenResolution), 1, 0)
 # It is a binary variable so we can encode it as 0 and 1. 
-# 1 means the laptop is a touch screen and 0 indicates not a touch screen.
+# 1 means the laptop is with a touch screen and 0 indicates not a touch screen.
 
 head(dataset) # we separated touchscreen data
+table(dataset$touchscreen) # value count
 
 #Number of Touchscreen laptop - have / not have
 barplot(table(dataset$touchscreen), main='Number of TouchScreen laptop',ylab = "No. of occurance",xlab = "TouchScreen", col='blue', breaks=12)
@@ -121,10 +122,10 @@ ggplot(dataset, aes(x = factor(touchscreen), y = Price, fill = Price, colour = P
 # By observing, touchscreen laptop price is high.
 
 # 2. Fetching IPS panel 
-dataset$Ips <- ifelse(grepl("IPS", dataset$ScreenResolution), 1, 0)
+dataset$Ips <- ifelse(grepl("IPS Panel", dataset$ScreenResolution), 1, 0)
 
 head(dataset)
-
+table(dataset$Ips)
 barplot(table(dataset$Ips), main='Number of IPS Panel laptop',ylab = "Number",xlab = "IPS Panel", col='blue', breaks=12)
 # Laptop with IPS Panel are less in our dataset.
 
@@ -175,6 +176,7 @@ glimpse(dataset)
 
 # Now, CPU column, does it affect Price?
 unique(dataset$Cpu)
+
 # Cpu have 117 different categories, which give information about pre-processor and 
 # speed of laptops.
 
@@ -209,6 +211,8 @@ head(dataset)
 barplot(table(dataset$Cpu_brand), main='Number of CPU brand',ylab = "Count",xlab = "CPU brand", col='blue', breaks=12)
 # Intel i7 and i5 are the most in the dataset.
 
+table(dataset$Cpu_brand) # value count
+
 # Will price vary based on cpu processor?
 
 # Relationship between Cpu brand and Price
@@ -235,12 +239,79 @@ ggplot(dataset, aes(x = factor(Ram), y = Price, fill = Price, colour = Price)) +
 
 # Memory vs Price?
 
-dataset$Memory # lots of information 
+# dataset$Memory # lots of information 
 unique(dataset$Memory) # what are the unique one - 39 chan
+table(dataset$Memory) # value count
 
 # Feature Engineering
 
+# many categories and some only have 1 (difficult to prediction)
+# some with both ssd and hdd, some have only ssd, some have hybrid, flash storage.
+# so, we separate the value in 4 column - 1. Hard drive 2. SSD 3. Flash Storage, 4. Hybrid
+
+# glimpse(dataset)
+
+# dataset$Memory <- gsub("\\.0", "", as.character(dataset$Memory))
+# #Replacing GB to ''(null or empty)
+# dataset$Memory <- gsub("GB", "", dataset$Memory)
+# #Replacing TB to 000 (1TB = 1000GB)
+# dataset$Memory <- gsub("TB", "000", dataset$Memory)
+# split 64GB Flash Storage +  1TB(000) HDD
+# new <- strsplit(dataset$Memory, "\\+")[1][2]
 
 
 
+#----------------------
 
+table(dataset$Gpu)
+# Many different graphics card information, but we only select brand name of
+# particular graphics card
+
+dataset$Gpubrand <- sapply(dataset$Gpu, function(x) {strsplit(x, " ")[[1]][1]})
+table(dataset$Gpubrand)
+dataset <- dataset[dataset$`Gpubrand` != 'ARM', ] # we don't need GPU ARM only brand name.
+table(dataset$Gpubrand)
+
+barplot(table(dataset$Gpubrand), main='count of GPU',ylab = "Count",xlab = "GPU", col='blue', breaks=12)
+# Intel GPU are most in dataset.
+
+# Relationship between GPU and Price
+ggplot(dataset, aes(x = factor(Gpubrand), y = Price, fill = Price, colour = Price)) + 
+  geom_bar(stat = "identity", position = "dodge")
+# Nvidia GPU are much expansive than Intel and AMD
+
+# Drop GPU column
+dataset <- dataset[, !(names(dataset) %in% c('Gpu'))]
+head(dataset)
+
+
+# Operating System
+
+table(dataset$OpSys) 
+# we can see, there is Mac OS x, MacOs | Windows 10 , Windows 10s
+# To make easy, we keep all Mac categories at one, same to windows
+cat_os <- function(os) {
+  if (os == 'Windows 10' || os == 'Windows 7' || os == 'Windows 10 S') {
+    return('Windows')
+  } else if (os == 'Mac OS X' || os == 'macOS') {
+    return('Mac')
+  } else {
+    return('others/NO OS/Linux')
+  }
+}
+
+dataset$OS <- sapply(dataset$OpSys, cat_os)
+head(dataset)
+table(dataset$OS) 
+
+# Drop OpSys column
+dataset <- dataset[, !(names(dataset) %in% c('OpSys'))]
+head(dataset)
+
+barplot(table(dataset$OS), main='count of Operating System',ylab = "Count",xlab = "OS", col='blue', breaks=12)
+# Intel GPU are most in dataset.
+
+# Relationship between GPU and Price
+ggplot(dataset, aes(x = factor(OS), y = Price, fill = Price, colour = Price)) + 
+  geom_bar(stat = "identity", position = "dodge")
+# windows laptop are more expansive compare to other's
